6dbd0954d2b7dd46ec2a3429d89103bc709d5ca1
Alma Isaksson <isakvent@outlook.com>
2024-01-25 16:16:45 +0000

    made test cases for project

--- project/tests.py
+++ project/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Project
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class ProjectListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='cherry', password='berrybasket')
+
+    def test_can_list_project(self):
+        testuser1 = User.objects.get(username='cherry')
+        Project.objects.create(owner=testuser1, project_title='on top')
+        response = self.client.get('/projects/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_login_in_user_can_create_project(self):
+        self.client.login(username='cherry', password='berrybasket')
+        response = self.client.post('/projects/', {'title': 'on top'})
+        count = Project.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_logout_user_cant_create_project(self):
+        response = self.client.post('/projects/', {'title': 'on top'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)

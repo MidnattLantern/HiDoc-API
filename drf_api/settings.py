d3fb4b28477460c0c7bbaa6cddf1fedc9a61fab2
Alma Isaksson <isakvent@outlook.com>
2024-01-31 11:48:27 +0000

    edit typo

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -38,7 +38,7 @@ REST_FRAMEWORK = {
     'DATETIME_FORMAT': '%d %b %Y',
 }
 if 'DEV' not in os.environ:
-    REST_FRAMEWORK['DEFAULT_RENDER_CLASSES'] = [
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
         'rest_framework.renderers.JSONRenderer',
     ]
 

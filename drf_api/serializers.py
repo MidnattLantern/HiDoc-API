d8ee42dc06af5b3f25aa6a662a496963bc4aef5a
Alma Isaksson <isakvent@outlook.com>
2024-01-31 17:17:08 +0000

    rename serializers from profile to artacccount

--- drf_api/serializers.py
+++ drf_api/serializers.py
@@ -3,10 +3,10 @@ from rest_framework import serializers
 
 
 class CurrentUserSerializer(UserDetailsSerializer):
-    profile_id = serializers.ReadOnlyField(source='profile.id')
-    profile_image = serializers.ReadOnlyField(source='profile.image.url')
+    artaccount_id = serializers.ReadOnlyField(source='profile.id')
+    artaccount_image = serializers.ReadOnlyField(source='profile.image.url')
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
-            'profile_id', 'profile_image'
+            'artaccount_id', 'artaccount_image'
         )

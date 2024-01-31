066e21f43fe131498058e161f16e259861f7f077
Alma Isaksson <isakvent@outlook.com>
2024-01-31 17:23:07 +0000

    change source inside serializers from profile to artaccount

--- drf_api/serializers.py
+++ drf_api/serializers.py
@@ -3,8 +3,8 @@ from rest_framework import serializers
 
 
 class CurrentUserSerializer(UserDetailsSerializer):
-    artaccount_id = serializers.ReadOnlyField(source='profile.id')
-    artaccount_image = serializers.ReadOnlyField(source='profile.image.url')
+    artaccount_id = serializers.ReadOnlyField(source='artaccount.id')
+    artaccount_image = serializers.ReadOnlyField(source='artaccount.image.url')
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (

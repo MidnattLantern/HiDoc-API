aea9f82edce9d6db388f61cb34c920f2abcdf16f
Alma Isaksson <isakvent@outlook.com>
2024-02-07 13:00:14 +0000

    user can edit and delete with generic views

--- project/views.py
+++ project/views.py
@@ -83,3 +83,37 @@ class ProjectDetail(generics.RetrieveAPIView):
             'watching_project', distinct=True
         ),
     ).order_by('-created_at')
+
+    def get_object(self, pk):
+        try:
+            project = Project.objects.get(pk=pk)
+            self.check_object_permissions(self.request, project)
+            return project
+        except Project.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        project = self.get_object(pk)
+        serializer = ProjectSerializer(
+            project, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        project = self.get_object(pk)
+        serializer = ProjectSerializer(
+            project, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        project = self.get_object(pk)
+        project.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )

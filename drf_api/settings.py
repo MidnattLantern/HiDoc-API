7b743dcfc58207a964afc63089367da6fb6747e8
Alma Isaksson <isakvent@outlook.com>
2024-03-11 10:49:02 +0000

    (completion) security patching

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -56,7 +56,7 @@ REST_AUTH_SERIALIZERS = {
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-50lazhzh3*=p1j4=#z9$*nis8(h^jd*5mrkj@7dm)zj0f*q=rl'
+
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
